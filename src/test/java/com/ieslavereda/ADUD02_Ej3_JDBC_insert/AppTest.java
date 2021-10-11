cce727330ba9235355cbd4fc3c231603c7652a4c
xrosi <xrosi@TorreRosillo>
2021-10-11 13:45:10 +0200

    1st commit

---
+++ src/test/java/com/ieslavereda/ADUD02_Ej3_JDBC_insert/AppTest.java
@@ -0,0 +1,20 @@
+package com.ieslavereda.ADUD02_Ej3_JDBC_insert;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
